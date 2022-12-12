1de8d8a4ce9aaee3a2dedb991ca62160c27de6b2
olafnub <googlhoogle@gmail.com>
2022-12-11 22:33:49 -0600

    Check to see if submission if empty

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
-
-window.onload = () => {
-    const addTerm = (ev) => {
-        ev.preventDefault();
-        
-        let addTerms = {
-            term: document.querySelector("#fterm").value,
-            definition: document.querySelector("#fdefi").value
-        }
+// Get definition for top half of card
+let storeTerms = [];
+const addTerm = (ev) => {
+    ev.preventDefault();
+    
+    let addTerms = {
+        term: document.querySelector("#fterm").value,
+        definition: document.querySelector("#fdefi").value
     }
 
-// get definition for top half of the card
-let getDefinition = document.querySelector("#definition");
-getDefinition.textContent = 'hi';
+// Check if term is "" -> Remove if nothing
+    if (addTerms.value == "" || addTerms.definition == "") {
+        alert('try again');
+    } else {
+    storeTerms += JSON.stringify(addTerms);
+    let logUserInput = document.querySelector("#termsAdded pre");
+    logUserInput.textContent += addTerms.term + "has been added\n";
+    }
+    document.forms[0].reset();
 
 
 }
+window.onload = () => {
+
+    document.getElementById("createButton").addEventListener('click', addTerm);
+    // let getDefinition = document.querySelector("#definition");
+    // getDefinition.textContent = addTerms.definition;
+}
+
 
 
