24ef9e78174f956bc1d3f19ba80176832efd321f
olafnub <googlhoogle@gmail.com>
2022-12-11 18:08:41 -0600

    Test out getting user input

---
+++ script.js
@@ -0,0 +1,19 @@
+
+window.onload = () => {
+    const addTerm = (ev) => {
+        ev.preventDefault();
+        
+        let addTerms = {
+            term: document.querySelector("#fterm").value,
+            definition: document.querySelector("#fdefi").value
+        }
+    }
+
+// get definition for top half of the card
+let getDefinition = document.querySelector("#definition");
+getDefinition.textContent = 'hi';
+
+
+}
+
+
